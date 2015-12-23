f2ba416de1724cb17f243dd8f556887d0f90f0a1
Slava Petrov <slavik--petrov@yandex.ru>
2015-12-23 16:24:45 +0300

    Add /visit functionality

--- app.rb
+++ app.rb
@@ -15,6 +15,27 @@ get '/visit' do
   erb :visit
 end
 
+post '/visit' do
+  @username = params[:username]
+  @phone = params[:phone]
+  @datetime = params[:datetime]
+  @barber = params[:barber]
+
+  err_hash = {:username => 'Введите имя',
+              :phone => 'Введите телефон',
+              :datetime => 'Введите дату и время'}
+
+  err_hash.each do |key, value|
+    if params[key] == ''
+      @error = err_hash[key]
+      return erb :visit
+    end
+  end
+
+  erb "Отлично, #{@username}, мастер #{@barber} будет Вас ждать в #{@datetime}"
+
+end
+
 get '/about' do
   erb :about
 end
