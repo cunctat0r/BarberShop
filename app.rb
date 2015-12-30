045ebaf4af5657d10e74fd8ec6b98dc96c81e8b7
Slava Petrov <slavik--petrov@yandex.ru>
2015-12-30 14:29:18 +0300

    Create sqlite database

--- app.rb
+++ app.rb
@@ -1,9 +1,19 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sqlite3'
 
 configure do
   enable :sessions
+  @db = SQLite3::Database.new 'barbershop.sqlite'
+  @db.execute 'CREATE TABLE IF NOT EXISTS 
+              "Users" 
+              ("id" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL , 
+                "username" TEXT, 
+                "phone" TEXT, 
+                "datestamp" TEXT, 
+                "barber" TEXT, "color" TEXT
+                )'
 end
 
 
